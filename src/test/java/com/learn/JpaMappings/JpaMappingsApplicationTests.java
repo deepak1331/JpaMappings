ebfd9feffb3d564b74ab990425b84d0c83a46878
Deepak Yadav <deepakyadav1331@gmail.com>
2025-08-22 17:55:04 +0530

    Initial Setup

---
+++ src/test/java/com/learn/JpaMappings/JpaMappingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.JpaMappings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaMappingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
